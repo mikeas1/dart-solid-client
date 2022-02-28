1b0d64051a74f4bb9845c0dd3396b487beb19767
Mike Smith <mike.smith1578@gmail.com>
2022-02-27 22:44:57 -0700

    Initialize new dart library

---
+++ example/solid_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:solid_client/solid_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/solid_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library solid_client;
+
+export 'src/solid_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
